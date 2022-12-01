af834306fecb38c34193581aa2b843c0953e32cd
Benjamin Le Berre <benjamin@le-berre.com>
2022-12-01 08:49:13 +0100

    day1 part2

--- day1.rs
+++ day1.rs
@@ -1,23 +1,31 @@
 use std::fs;
-use std::cmp;
 
 fn main() {
     let input = fs::read_to_string("day1_input.txt")
         .expect("Should have been able to read the input");
 
-    let mut max: u64 = 0;
+    // Keep sorted such that max[2] <= max[1] <= max[0]
+    let mut max: [u64; 3] = [0, 0, 0];
     let mut sum: u64 = 0;
 
     for n in input.split("\n") {
         if n.is_empty() {
-            max = cmp::max(max, sum);
+            if max[0] < sum {
+                max[2] = max[1];
+                max[1] = max[0];
+                max[0] = sum;
+            } else if max[1] < sum {
+                max[2] = max[1];
+                max[1] = sum;
+            } else if max[2] < sum {
+                max[2] = sum;
+            }
+
             sum = 0;
             continue;
         }
 
         sum += n.parse::<u64>().unwrap();
     }
-
-    max = cmp::max(max, sum);
-    println!("max is {}", max);
+    println!("max is [{}, {}, {}] summed is {}", max[0], max[1], max[2], max[0] + max[1] + max[2]);
 }
