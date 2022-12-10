7c27ff6eb894bb50fdd64ee20e94de627dd898ea
Benjamin Le Berre <benjamin@le-berre.com>
2022-12-10 13:30:10 +0100

    cargo fmt

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -16,9 +16,7 @@ fn main() {
     println!("2: {}", solve_part2(input));
 }
 
-#[derive(PartialEq)]
-#[derive(Clone)]
-#[derive(Copy)]
+#[derive(PartialEq, Clone, Copy)]
 struct Point {
     x: i64,
     y: i64,
@@ -31,18 +29,18 @@ fn unroll_action(action: &str) -> std::iter::Take<std::iter::Repeat<&str>> {
 fn solve_part1(input: &str) -> usize {
     let actions = input.lines().flat_map(unroll_action);
 
-    let mut tail_positions = vec![Point{ x: 0, y: 0 }];
+    let mut tail_positions = vec![Point { x: 0, y: 0 }];
     let mut head = Point { x: 0, y: 0 };
     let mut tail = head;
 
     for direction in actions {
         let mut new_head = head;
         match direction {
-            "U" => {new_head.y += 1}
-            "D" => {new_head.y -= 1}
-            "R" => {new_head.x += 1}
-            "L" => {new_head.x -= 1}
-            _ => panic!()
+            "U" => new_head.y += 1,
+            "D" => new_head.y -= 1,
+            "R" => new_head.x += 1,
+            "L" => new_head.x -= 1,
+            _ => panic!(),
         }
         if 2 <= tail.x.abs_diff(new_head.x) || 2 <= tail.y.abs_diff(new_head.y) {
             tail = head;
@@ -57,28 +55,32 @@ fn solve_part1(input: &str) -> usize {
 }
 
 fn knot_distance(a: &Point, b: &Point) -> Point {
-    Point{ x: a.x - b.x, y: a.y - b.y }
+    Point {
+        x: a.x - b.x,
+        y: a.y - b.y,
+    }
 }
 
 fn solve_part2(input: &str) -> usize {
     let actions = input.lines().flat_map(unroll_action);
 
-    let mut tail_positions = vec![Point{ x: 0, y: 0 }];
-    let mut knots = [Point{ x: 0, y:0 }; 10];
+    let mut tail_positions = vec![Point { x: 0, y: 0 }];
+    let mut knots = [Point { x: 0, y: 0 }; 10];
 
     for direction in actions {
         let movement = match direction {
-            "U" => Point{ x:1, y:0 },
-            "D" => Point{ x:-1, y:0 },
-            "R" => Point{ x:0, y:1 },
-            "L" => Point{ x:0, y:-1 },
-            _ => panic!()
+            "U" => Point { x: 1, y: 0 },
+            "D" => Point { x: -1, y: 0 },
+            "R" => Point { x: 0, y: 1 },
+            "L" => Point { x: 0, y: -1 },
+            _ => panic!(),
         };
         knots[0].x += movement.x;
         knots[0].y += movement.y;
 
-        for i in 1..10 { // second until last
-            let dist = knot_distance(&knots[i-1], &knots[i]);
+        for i in 1..10 {
+            // second until last
+            let dist = knot_distance(&knots[i - 1], &knots[i]);
             if 2 < dist.x.abs() && 2 < dist.y.abs() {
                 panic!();
             } else if dist.x.abs() == 2 || dist.y.abs() == 2 {
