06c6a7b55fbcd7ed4d090d0d405e34bd183ffb7f
Benjamin Le Berre <benjamin@le-berre.com>
2022-12-16 21:33:25 +0100

    prepare templates

---
+++ src/bin/day19.rs
@@ -0,0 +1,12 @@
+#[test]
+fn test() {
+    let input = include_str!("day19_test.txt");
+    assert_eq!(solve_part1(input), 0);
+    //assert_eq!(solve_part2(input), 0);
+}
+
+fn main() {
+    let input = include_str!("day19_input.txt");
+    println!("1: {}", solve_part1(input));
+    //println!("2: {}", solve_part1(input));
+}
